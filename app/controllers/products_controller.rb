ebb4e32f71d1c786d43ae4a0c30973b9d6e5da76
markprubin <mark.p.rubin@gmail.com>
2022-11-02 10:48:58 -0700

    displays json for all products and individual products, adds routes for each individual product

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,5 +1,29 @@
 class ProductsController < ApplicationController
   def all_products
-    render json: Product.all
+    products = Product.all
+    render json: products.as_json
+    #render json: Product.all (why does this also work?)
   end
+
+  def shovel
+    product = Product.find_by(name: "Heavy Duty Wide Shovel")
+    render json: product.as_json
+  end
+
+  def lawnmower
+    product = Product.find_by(name: "Electric Lawnmower")
+    render json: product.as_json
+  end
+
+  def rake
+    product = Product.find_by(name: "18 in. Gravel Rake")
+    render json: product.as_json
+  end
+
+  def glove
+    product = Product.find_by(name: "Heavy Duty Work Gloves")
+    render json: product.as_json
+  end
+
+  
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
 get "/product_path", controller: "products", action: "all_products"
+
+get "/shovel_path", controller: "products", action: "shovel"
+
+get "/rake_path", controller: "products", action: "rake"
+
+get "/glove_path", controller: "products", action: "glove"
 end
