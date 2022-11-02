d2a4ce3586ec1b4a08940dbaea33f056e1bbed35
markprubin <mark.p.rubin@gmail.com>
2022-11-02 12:00:50 -0700

    updates routes by :id on one path and updates controller to find by :id

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,28 +1,8 @@
 class ProductsController < ApplicationController
   def all_products
-    products = Product.all
+    products = Product.find_by(id: params["id"])
     render json: products.as_json
     #render json: Product.all (why does this also work?)
-  end
-
-  def shovel
-    product = Product.find_by(name: "Heavy Duty Wide Shovel")
-    render json: product.as_json
-  end
-
-  def lawnmower
-    product = Product.find_by(name: "Electric Lawnmower")
-    render json: product.as_json
-  end
-
-  def rake
-    product = Product.find_by(name: "18 in. Gravel Rake")
-    render json: product.as_json
-  end
-
-  def glove
-    product = Product.find_by(name: "Heavy Duty Work Gloves")
-    render json: product.as_json
   end
 
   
--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,5 @@
 Rails.application.routes.draw do
-get "/product_path", controller: "products", action: "all_products"
+get "/product_path/:id", controller: "products", action: "all_products"
 
-get "/shovel_path", controller: "products", action: "shovel"
 
-get "/lawnmower_path", controller: "products", action: "lawnmower"
-
-get "/rake_path", controller: "products", action: "rake"
-
-get "/glove_path", controller: "products", action: "glove"
 end
