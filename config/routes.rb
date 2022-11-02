8fe56bd3c082df64d2dfcdbcc0f819f3967a03a6
markprubin <mark.p.rubin@gmail.com>
2022-11-02 11:45:37 -0700

    adds frontend.rb and updates routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,8 @@ get "/product_path", controller: "products", action: "all_products"
 
 get "/shovel_path", controller: "products", action: "shovel"
 
+get "/lawnmower_path", controller: "products", action: "lawnmower"
+
 get "/rake_path", controller: "products", action: "rake"
 
 get "/glove_path", controller: "products", action: "glove"
