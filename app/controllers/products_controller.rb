db6998d78f2a8828ea4c7a005e3d13735eb24110
markprubin <mark.p.rubin@gmail.com>
2022-11-03 11:44:45 -0700

    edits create action with params and adds params to POST request

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -13,10 +13,10 @@ class ProductsController < ApplicationController
 
   def create
     product = Product.new(
-      name: "Soil Topper",
-      price: 7,
-      image_url: "https://images.thdstatic.com/productImages/867ace64-31e5-4940-93c8-92a192532964/svn/kellogg-garden-organics-topsoil-6560-64_1000.jpg",
-      description: "Topper for lawns. Enriched with organic fertilizer. Helps seeds germinate faster. Helps revitalize lawns.",
+      name: params["name"],
+      price: params["price"],
+      image_url: params["image_url"],
+      description: params["description"],
     )
       product.save
       render json: product.as_json
