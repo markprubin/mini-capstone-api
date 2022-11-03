18cec9f18b363e7782432c170b0a7f6cb67f9fc6
markprubin <mark.p.rubin@gmail.com>
2022-11-03 09:46:33 -0700

    updates frontend2

--- frontend2.rb
+++ frontend2.rb
@@ -4,37 +4,41 @@ system "clear"
 
 # MOVE TO PRACTICE WEB REQUESTS
 
-# response = HTTP.get("http://localhost:3000/product_path")
+# response = HTTP.get("http://localhost:3000/products")
 #   products = response.parse["name"]
-#   pp products
 
 
-# puts "Welcome to the Landscaping Store. The items below are currently in stock:"
-# puts "[1] Heavy Duty Wide Shovel"
-# puts "[2] Electric Lawnmower"
-# puts "[3] 18 in. Gravel Rake"
-# puts "[4] Heavy Duty Work Gloves"
 
-# input = gets.chomp
+puts "Welcome to the Landscaping Store. The items below are currently in stock:"
+puts "[1] Heavy Duty Wide Shovel"
+puts "[2] Electric Lawnmower"
+puts "[3] 18 in. Gravel Rake"
+puts "[4] Heavy Duty Work Gloves"
 
-# if input == "1"
-#   response = HTTP.get("http://localhost:3000/product_path/1")
-#   product = response.parse["name"]
-#   pp product
-# end
-# if input == "2"
-#   response = HTTP.get("http://localhost:3000/product_path/2")
-#   product = response.parse["name"]
-#   pp product
-# end
-# if input == "3"
-#   response = HTTP.get("http://localhost:3000/product_path/3")
-#   product = response.parse["name"]
-#   pp product
-# end
-# if input == "4"
-#   response = HTTP.get("http://localhost:3000/product_path/4")
-#   product = response.parse["name"]
-#   pp product
-# end
+input = gets.chomp
+
+if input == "1"
+  response = HTTP.get("http://localhost:3000/products/1")
+  product = response.parse["name"]
+  price = response.parse["price"]
+  puts "The #{product} costs $#{price}."
+end
+if input == "2"
+  response = HTTP.get("http://localhost:3000/products/2")
+  product = response.parse["name"]
+  price = response.parse["price"]
+  puts "The #{product} costs $#{price}."
+end
+if input == "3"
+  response = HTTP.get("http://localhost:3000/products/3")
+  product = response.parse["name"]
+  price = response.parse["price"]
+  puts "The #{product} costs $#{price}."
+end
+if input == "4"
+  response = HTTP.get("http://localhost:3000/products/4")
+  product = response.parse["name"]
+  price = response.parse["price"]
+  puts "The #{product} costs $#{price}."
+end
 
