2ef2672c82a32e827ef5c0f8a71d3090b6bfaae3
markprubin <mark.p.rubin@gmail.com>
2022-11-03 10:47:36 -0700

    adds index show and create requests and create action in controller

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -11,5 +11,14 @@ class ProductsController < ApplicationController
     #render json: Product.all (why does this also work?)
   end
 
-
+  def create
+    product = Product.new(
+      name: "Soil Topper",
+      price: 7,
+      image_url: "https://images.thdstatic.com/productImages/867ace64-31e5-4940-93c8-92a192532964/svn/kellogg-garden-organics-topsoil-6560-64_1000.jpg",
+      description: "Topper for lawns. Enriched with organic fertilizer. Helps seeds germinate faster. Helps revitalize lawns.",
+    )
+      product.save
+      render json: product.as_json
+  end
 end
