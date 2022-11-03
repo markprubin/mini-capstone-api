993c5a5ce1a3d17dce0a894e44570d1b4fdc2ce1
markprubin <mark.p.rubin@gmail.com>
2022-11-03 09:15:17 -0700

    updates links in responses

--- frontend2.rb
+++ frontend2.rb
@@ -2,39 +2,39 @@ require "http"
 
 system "clear"
 
-#MOVE TO PRACTICE WEB REQUESTS
+# MOVE TO PRACTICE WEB REQUESTS
 
-# response = HTTP.get("https://http://localhost:3000/product_path")
+# response = HTTP.get("http://localhost:3000/product_path")
 #   products = response.parse["name"]
 #   pp products
 
 
-puts "Welcome to the Landscaping Store. The items below are currently in stock:"
-puts "[1] Heavy Duty Wide Shovel"
-puts "[2] Electric Lawnmower"
-puts "[3] 18 in. Gravel Rake"
-puts "[4] Heavy Duty Work Gloves"
+# puts "Welcome to the Landscaping Store. The items below are currently in stock:"
+# puts "[1] Heavy Duty Wide Shovel"
+# puts "[2] Electric Lawnmower"
+# puts "[3] 18 in. Gravel Rake"
+# puts "[4] Heavy Duty Work Gloves"
 
-input = gets.chomp
+# input = gets.chomp
 
-if input == "1"
-  response = HTTP.get("https://http://localhost:3000/product_path/1")
-  product = response.parse["name"]
-  pp product
-end
-if input == "2"
-  response = HTTP.get("https://http://localhost:3000/product_path/2")
-  product = response.parse["name"]
-  pp product
-end
-if input == "3"
-  response = HTTP.get("https://http://localhost:3000/product_path/3")
-  product = response.parse["name"]
-  pp product
-end
-if input == "4"
-  response = HTTP.get("https://http://localhost:3000/product_path/4")
-  product = response.parse["name"]
-  pp product
-end
+# if input == "1"
+#   response = HTTP.get("http://localhost:3000/product_path/1")
+#   product = response.parse["name"]
+#   pp product
+# end
+# if input == "2"
+#   response = HTTP.get("http://localhost:3000/product_path/2")
+#   product = response.parse["name"]
+#   pp product
+# end
+# if input == "3"
+#   response = HTTP.get("http://localhost:3000/product_path/3")
+#   product = response.parse["name"]
+#   pp product
+# end
+# if input == "4"
+#   response = HTTP.get("http://localhost:3000/product_path/4")
+#   product = response.parse["name"]
+#   pp product
+# end
 
