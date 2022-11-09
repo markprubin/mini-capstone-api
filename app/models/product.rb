7387443a7860589ba0c54b7d3201c238a5cdaf50
markprubin <mark.p.rubin@gmail.com>
2022-11-09 14:06:27 -0700

    adds new image url

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,16 +1,16 @@
 class Product < ApplicationRecord
-  has_many :images
-  belongs_to :supplier
-  
+
   # validates :name, presence: true, uniqueness: true
   # validates :price, presence: true, numericality: { greater_than: 0 }
   # validates :description, presence: true, length: { in: 10..500 }
 
             
-
+  belongs_to :supplier
   # def supplier
   #   Supplier.find_by(id: supplier_id)
   # end
+
+  has_many :images
 
   # def images
   #   Image.where(product_id: id)
