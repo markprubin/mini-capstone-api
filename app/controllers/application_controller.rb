f3012528a95e95d7050b7fda0bb16ee4d4b342de
markprubin <mark.p.rubin@gmail.com>
2022-11-11 12:01:42 -0700

    updates create

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -23,7 +23,7 @@ class ApplicationController < ActionController::API
     end
   end
 
-  
+
 
 
 end
--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -11,12 +11,21 @@ class OrdersController < ApplicationController
   end
 
   def create
+    product = Product.find_by(id: params[:product_id])
+
+    calc_subtotal = product.price * params[:quantity].to_i
+    calc_tax = calc_subtotal * 0.09
+    calc_total = calc_subtotal + calc_tax
+
     order = Order.new(
       user_id: current_user.id,
       product_id: params[:product_id],
       quantity: params[:quantity],
-      subtotal: 
+      subtotal: calc_subtotal,
+      tax: calc_tax,
+      total: calc_total,
     )
+
     if order.save
       render json: order.as_json
     else
